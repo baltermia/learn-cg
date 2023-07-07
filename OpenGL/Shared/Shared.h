79e8aef92bb105ad0f023e07db7b8c70cc04ced3
baltermia <54800864+baltermia@users.noreply.github.com>
2023-07-07 08:51:07 +0200

    Create shrd::Vec3 datatype

--- OpenGL/Shared/Shared.h
+++ OpenGL/Shared/Shared.h
@@ -3,6 +3,7 @@
 #include "IO.h"
 #include "Init.h"
 #include "BasicOpenGLSetup.h"
+#include "Vec.h"
 
 /// <summary>
 /// The Shared-Namespace (abbreviated 'shrd') contains common helper functions and classes that are widely reusable 
---
+++ OpenGL/Shared/Vec.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace shrd
+{
+
+	template<typename T>
+	struct Vec3
+	{
+		T x, y, z;
+	};
+
+}
