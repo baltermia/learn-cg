c892710fc507ee799109d7cb9aafeaae0a35d13c
baltermia <54800864+baltermia@users.noreply.github.com>
2023-07-07 13:08:51 +0200

    Fully implement TwoTriangles2

--- OpenGL/LearnOpenGL.com/Excercises/Two Triangles 2/main.cpp
+++ OpenGL/LearnOpenGL.com/Excercises/Two Triangles 2/main.cpp
@@ -1,6 +1,17 @@
 #include "TwoTriangles2.h"
 
+/*
+ * https://learnopengl.com/Getting-started/Hello-Triangle
+ *
+ * Excercise 2:
+ *
+ * Now create the same 2 triangles using two different VAOs and VBOs for their data.
+*/
+
 int main()
 {
-
+	TwoTriangles2 setup;
+	setup.initialize();
+	setup.run();
+	setup.clean();
 }
