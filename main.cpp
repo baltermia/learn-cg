4523e43f71394407680844652e4976ad625544a9
baltermia <54800864+baltermia@users.noreply.github.com>
2023-11-03 21:33:29 +0100

    See if I still got it

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
+    std::vector<int> v = { 1, 3, 4, 6 };
+
+    for (const int& i : v)
+    {
+        std::cout << i << std::endl;
+    }
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
