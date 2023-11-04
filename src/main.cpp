67260fa69c2f90b8d95b18da69a88d5b4413275d
baltermia <54800864+baltermia@users.noreply.github.com>
2023-11-04 14:15:47 +0100

    Add glfw code to open window

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,37 @@
-#include <iostream>
-#include <vector>
+#include <GLFW/glfw3.h>
 
-int main()
+int main(void)
 {
-    std::vector<int> v = { 1, 3, 4, 6 };
+    GLFWwindow* window;
 
-    for (const int& i : v)
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
     {
-        std::cout << i << std::endl;
+        glfwTerminate();
+        return -1;
     }
 
-    std::cout << "Hello, World!" << std::endl;
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        // glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
